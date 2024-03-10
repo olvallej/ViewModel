c567fbbb856217b2c563c85b11c6e3d0efd7e629
olvallej <olvallej@unirioja.es>
2024-03-10 22:57:09 +0100

    GameScreen creado

--- app/src/main/java/com/example/viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.viewmodel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,33 +15,18 @@ import com.example.viewmodel.ui.theme.ViewModelTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             ViewModelTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    GameScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ViewModelTheme {
-        Greeting("Android")
-    }
-}
